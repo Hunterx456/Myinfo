a025f6c15bfcb2c5e60f057b3b317384f6354786
Hunterx456 <150821453+Hunterx456@users.noreply.github.com>
2024-01-17 21:21:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    console.log("Email: " + email);
+    console.log("Password: " + password);
+}
