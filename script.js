62c602b3586d4ac5ed156490a78ef81fcf86e8f1
Hunterx456 <150821453+Hunterx456@users.noreply.github.com>
2024-01-17 22:50:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function login() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    console.log("Email: " + email);
-    console.log("Password: " + password);
-}
+function login(event) {
+    event.preventDefault();
+  
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const loginMessage = document.getElementById('loginMessage');
+  
+    if (email === 'hello' && password === 'hello') {
+      window.location.href = 'myinfo.html';
+    } else {
+      loginMessage.textContent = 'Wrong username or password. Please try again.';
+    }
+  }
+  
