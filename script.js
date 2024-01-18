34571ffe0970303f7af854a36ab9bd1431850c93
Hunterx456 <150821453+Hunterx456@users.noreply.github.com>
2024-01-18 20:16:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function login(event) {
-    event.preventDefault();
-  
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const loginMessage = document.getElementById('loginMessage');
-  
-    if (email === 'hello' && password === 'hello') {
-      window.location.href = 'myinfo.html';
-    } else {
-      loginMessage.textContent = 'Wrong username or password. Please try again.';
-    }
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const loginMessage = document.getElementById('loginMessage');
+  const loginButton = document.querySelector('button');
+
+  if (email === 'hello' && password === 'hello') {
+    window.location.href = 'myinfo.html';
+  } else {
+    loginMessage.textContent = 'Wrong username or password. Please try again.';
+
+    // Move the button to a random position
+    const randomX = Math.floor(Math.random() * 400);
+    const randomY = Math.floor(Math.random() * 400);
+
+    loginButton.style.position = 'absolute';
+    loginButton.style.left = `${randomX}px`;
+    loginButton.style.top = `${randomY}px`;
   }
-  
+}
